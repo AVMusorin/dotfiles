6985f4beaeaa289a76dff7cac12ad75c139b6692
Aleksandr Musorin <smusorin@mail.ru>
2025-12-30 12:37:06 +0100

    nvim: speed up clangd start up for ClickHouse project

--- .config/nvim/lua/user/lsp/settings/clangd.lua
+++ .config/nvim/lua/user/lsp/settings/clangd.lua
@@ -1,7 +1,14 @@
 return {
-  default_config = {
-    cmd = { 'clangd' },
-    filetypes = { 'c', 'cpp' },
-    single_file_support = true,
-  }
+  cmd = {
+    'clangd',
+    '--background-index=false',
+    '--completion-style=detailed',
+    '--header-insertion=never',
+    '--pch-storage=memory',
+    '--clang-tidy=false',         -- Disable slow clang-tidy checks
+    '-j=4',                       -- Use only 4 threads instead of all cores
+    '--malloc-trim',              -- Reduce memory usage
+  },
+  filetypes = { 'c', 'cpp' },
+  single_file_support = true,
 }
